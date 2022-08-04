2188233fbb029158d8f4a983d89501c049e53d15
julian <juliandecastelli@gmail.com>
2022-08-04 13:22:29 -0300

    add express-fileupload dependencie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
+const upload = require("express-fileupload");
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -9,12 +10,23 @@ mongoose.connect(process.env.MONGO_URI, {
 });
 
 var app = express();
+app.use(upload());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+app.post("/api/fileanalyse/", (req, res) => {
+  console.log(req.files.upfile.toString("utf8"));
+  res.json({ h: "hello" });
 });
 
 const port = process.env.PORT || 3000;
