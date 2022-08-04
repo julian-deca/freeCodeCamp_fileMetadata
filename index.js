af4362777a22dbb8b5d41a171c769510239c0e79
julian <juliandecastelli@gmail.com>
2022-08-04 13:29:19 -0300

    finish File Metadata project

--- index.js
+++ index.js
@@ -2,31 +2,17 @@ var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
 const upload = require("express-fileupload");
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 var app = express();
 app.use(upload());
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-
-app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 app.post("/api/fileanalyse/", (req, res) => {
-  console.log(req.files.upfile.toString("utf8"));
-  res.json({ h: "hello" });
+  const file = req.files.upfile;
+  res.json({ name: file.name, type: file.mimetype, size: file.size });
 });
 
 const port = process.env.PORT || 3000;
